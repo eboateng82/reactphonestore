af57da4005ed03898dcf48233517a7ef9b1bb1bf
enoch boateng <enochboateng82@gmail.com>
2020-10-12 09:55:10 -0700

    first react assignment

---
+++ src/data.js
@@ -0,0 +1,113 @@
+import React from 'react'
+
+export const storeProducts = [
+  {
+    id:1,
+    title:"Apple iPhone 11 Pro",
+    image:"images/Apple-iPhone-11-Pro-Max-Midnight-Green-frontimage.jpg",
+    price:1099,
+    company:"APPLE",
+    info:
+    "The successor of the iPhone XS comes with a compact 5.8” Super Retina XDR display, which is brighter and displays wider color gamut. There’s a triple camera setup - a ultra wide-angle F2.4 snapper joins the F1.8 regular and F2.0 telephoto cameras. Aside from great landscape shots, you will also be able to seamlessly switch between all three cameras while shooting videos. The new Deep Fusion camera feature brings HDR to another level by analyzing and combining 9 shots into one. The 7nm A13 Bionic chip provides a 20% increase in CPU and GPU performance, as well as improved battery life - up to 4 hours more. The iPhone 11 Pro is available in black, white, gold, and midnight green.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:2,
+    title:"Apple iPhone SE 2020",
+    image:"images/Apple-iPhone-SE.jpg",
+    price:949,
+    company:"APPLE",
+    info:
+    "Apple iPhone SE (2020) is a powerful yet pocket-sized iPhone. The new iPhone SE features an iPhone 8-style design with a 4.7-inch LCD display and Touch ID. The rear camera is a 12-megapixel one, while the selfie shooter is 7MP. The phone is powered by Apple's A13 Bionic chip combined with 64GB of internal storage as standard, although 128GB and 256GB variants are also available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:3,
+    title:"LG Aristo 5",
+    image:"images/LG-Aristo-5-Silver-frontimage.png",
+    price:849,
+    company:"LG",
+    info:'The LG Aristo 5 comes with a 90Hz 5.7" OLED display, a dual camera, a Snapdragon 855 chipset, 6GB of RAM, 64GB of storage, a 2800mAh battery, a dual-camera (wide + telephoto), and availability across all major US carriers. It has no fingerprint sensor save for a radar-powered face unlock method. You can also control certain apps and functions of the devices with simple gestures in front of the radar sensor.',
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:4,
+    title:"LG Stylo 6",
+    image:"images/LG-Stylo-6-White-Pearl-frontimage.png",
+    price:799,
+    company:"LG",
+    info:
+    "The LG Stylo 6 supports 5G thanks to the Snapdragon 865 chipset onboard and also comes with 8GB RAM and 128GB expandable storage. There are three cameras at the back, as well as a punch-hole selfie unit. The phone is powered by a 5000mAh battery and runs Android 10. The LG V60 is available from all large U.S. wireless providers.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:5,
+    title:"Nokia 5-3",
+    image:"images/nokia-5-3_1x.png",
+    price:799,
+    company:"NOKIA",
+    info:
+    "The Nokia 5-3 is a 5G-ready device that comes with a 6.78-inch QHD+ AMOLED display with 120Hz refresh rate, a Snapdragon 865 chipset with either 8 or 12GB of RAM, as well as fast storage with either 128 or 256GB of capacity. There's a 4,510mAh battery, and a quad-camera setup. There's official IP certification, as well as wireless charging, for the first time on an OnePlus phone.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:6,
+    title:"Samsung Galaxy A10e",
+    image:"images/Samsung-Galaxy-A10e-Black-frontimage.jpg",
+    price:950,
+    company:"SAMSUNG",
+    info:
+    "Equipped with a smaller display with a lower resolution, the Galaxy Note 10 serves as the entry-level to Samsung’s 2019 Note lineup. Aside from that, it still has the same hardware internals as the bigger--and more expensive--members of the Note line: a Snapdragon 855/Exynos 9825 chip, 8GB RAM, an S Pen, a centered punch-hole selfie camera, and an ultrasonic fingerprint scanner. Three cameras can be found at the back - 12MP main and telephoto, and a 16MP wide-angle snapper. There’s only one storage version with 256GB of native storage and no microSD card slot. The Note 10 is available in Aura Black, Aura White, Aura Glow, and the exclusive Aura Red and Aura Pink.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:7,
+    title:"Samsung Galaxy S20",
+    image:"images/Samsung-Galaxy-s20-plus-5g-y2-cosmicgray.jpg",
+    price:850,
+    company:"SAMSUNG",
+    info:
+    "The Samsung Galaxy S20+ comes with a Snapdragon 865/Exynos 990 chipset, 12GB RAM and 128GB/256GB/512GB of storage, as well as a 4,500mAh battery. Quad-camera setup is on the back of the phone: a 12MP primary sensor, an ultra-wide-angle camera, a telephoto camera, and a Time of Flight sensor.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:8,
+    title:"Nokia 7-2",
+    image:"images/nokia-7-2_1x.png",
+    price:879,
+    company:"NOKIA",
+    info:
+    'New Nokia 7.2 TA-1196 128GB Dual SIM GSM Factory Unlocked Android One 6.3" IPS LCD Display 6GB RAM Triple Camera w/ Zeiss Optics Smartphone',
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct =
+  {
+    id:1,
+    title:"Apple iPhone 11 Pro",
+    image:"images/Apple-iPhone-11-Pro-Max-Midnight-Green-frontimage.jpg",
+    price:1040,
+    company:"APPLE",
+    info:
+    "The successor of the iPhone XS comes with a compact 5.8” Super Retina XDR display, which is brighter and displays wider color gamut. There’s a triple camera setup - a ultra wide-angle F2.4 snapper joins the F1.8 regular and F2.0 telephoto cameras. Aside from great landscape shots, you will also be able to seamlessly switch between all three cameras while shooting videos. The new Deep Fusion camera feature brings HDR to another level by analyzing and combining 9 shots into one. The 7nm A13 Bionic chip provides a 20% increase in CPU and GPU performance, as well as improved battery life - up to 4 hours more. The iPhone 11 Pro is available in black, white, gold, and midnight green.",
+    inCart: false,
+    count: 0,
+    total: 0
+  };
